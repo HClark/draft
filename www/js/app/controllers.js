cc80286dcf69bcd766b3a6ba1671ad42732a0a44
Hannah M. Clark <hannahclark@hmclarkk-3.local>
2015-04-16 15:04:59 -0400

    first commit

---
+++ www/js/app/controllers.js
@@ -0,0 +1,32 @@
+/**
+ * beginnings of a controller to login to system
+ * here for the purpose of showing how a service might
+ * be used in an application
+ */
+angular.module('app.controllers', [])
+    .controller('ListDetailCtrl', [
+        '$state', '$scope', '$stateParams', 'UserService',   // <-- controller dependencies
+        function ($state, $scope, $stateParams, UserService) {
+
+            $scope.index = $stateParams.itemId;
+
+        }])
+    .controller('ListCtrl', [
+        '$state', '$scope', 'UserService',   // <-- controller dependencies
+        function ($state, $scope, UserService) {
+
+            $scope.dataList = ["One", "Two", "Three"];
+
+
+        }])
+    .controller('AccountCtrl', [
+        '$state', '$scope', 'UserService',   // <-- controller dependencies
+        function ($state, $scope, UserService) {
+
+            debugger;
+            UserService.currentUser().then(function (_user) {
+                $scope.user = _user;
+            });
+
+
+        }]);
